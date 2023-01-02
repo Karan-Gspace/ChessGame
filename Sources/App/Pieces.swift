6475bdff7eac887e2e29a181be999f76edcc9a73
Karan Naidu <karan.naidu@gspacetech.com>
2023-01-03 08:34:21 +1000

    Converted to a protocol.

--- Sources/App/Pieces.swift
+++ Sources/App/Pieces.swift
@@ -3,9 +3,9 @@
 /// The pieces are an essential part to the game of chess. Their positions on the
 /// board and coordination dictate the behaviour of the game. The Pieces type
 /// stores the rules for each of the pieces and the valid moves of those pieces.
-struct Pieces {
+protocol Pieces {
 
     /// The available positions where the piece can move on the board.
-    var validMoves: [String] { get }
+    var validMoves: [Pieces] { get }
 
 }
