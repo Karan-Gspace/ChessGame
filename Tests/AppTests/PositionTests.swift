8c97ec9fe1aed9e31223c9a14db94c3ecc35eb89
Karan Naidu <karan.naidu@gspacetech.com>
2023-01-04 18:39:36 +1000

    Added 3 unit tests for Position.

--- Tests/AppTests/PositionTests.swift
+++ Tests/AppTests/PositionTests.swift
@@ -8,12 +8,39 @@ import class Foundation.Bundle
 final class PositionTests: XCTestCase {
 
     /// Test init.
-    func testInit() throws {}
+    func testInit() throws {
+        let position = Position(0, 0)
+        XCTAssertEqual(position.x, 0)
+        XCTAssertEqual(position.y, 0)
+    }
 
     /// Test Equality.
-    func testEquality() throws {}
+    func testEquality() throws {
+        let position1 = Position(0, 0)
+        var position2 = Position(0, 0)
+        XCTAssertEqual(position1.x, position2.x)
+        XCTAssertEqual(position1.y, position2.y)
+        position2.x = 1
+        position2.y = 1
+        XCTAssertNotEqual(position1.x, position2.x)
+        XCTAssertNotEqual(position1.y, position2.y)
+    }
 
     /// Test getters and setters.
-    func testGettersAndSetters() throws {}
+    func testGettersAndSetters() throws {
+        let xInput = 0
+        let yInput = 0
+        let position = Position(0, 0)
+        XCTAssertEqual(position.x, xInput)
+        XCTAssertEqual(position.y, yInput)
+        let newXInput = 1
+        let newYInput = 1
+        XCTAssertNotEqual(position.x, newXInput)
+        XCTAssertNotEqual(position.y, newYInput)
+        position.x = newXInput
+        position.y = newYInput
+        XCTAssertEqual(position.x, newXInput)
+        XCTAssertEqual(position.y, newYInput)
+    }
 
 }
