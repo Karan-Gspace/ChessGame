e932d98e630936608d6aee2a1f22342242ae01b9
Karan Naidu <karan.naidu@gspacetech.com>
2023-01-04 18:32:46 +1000

    Created tests for chessPieces and Position.

---
+++ Tests/AppTests/PositionTests.swift
@@ -0,0 +1,19 @@
+import XCTest
+
+@testable import App
+
+import class Foundation.Bundle
+
+/// Tests for the Chess Pieces.
+final class PositionTests: XCTestCase {
+
+    /// Test init.
+    func testInit() throws {}
+
+    /// Test Equality.
+    func testEquality() throws {}
+
+    /// Test getters and setters.
+    func testGettersAndSetters() throws {}
+
+}
