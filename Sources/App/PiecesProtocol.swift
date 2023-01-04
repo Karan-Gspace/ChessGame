de61836193331de80bab91d34e244f83fda45de4
Karan Naidu <karan.naidu@gspacetech.com>
2023-01-04 18:09:22 +1000

    Abstract protocol.

--- Sources/App/PiecesProtocol.swift
+++ Sources/App/PiecesProtocol.swift
@@ -1,4 +1,4 @@
-/// The pieces type defines all the pieces to be used on the chess board.
+/// The pieces type defines all the pieces to be used on a gameboard board.
 ///
 /// The pieces are an essential part to the game of chess. Their positions on the
 /// board and coordination dictate the behaviour of the game. The Pieces type
