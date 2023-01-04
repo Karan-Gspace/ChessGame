d18563b9dac503ecde3aab64f052724c1079bdb1
Karan Naidu <karan.naidu@gspacetech.com>
2023-01-04 16:57:33 +1000

    Renamed pieces to piecesProtocol.

--- Sources/App/PiecesProtocol.swift
+++ Sources/App/PiecesProtocol.swift
@@ -3,9 +3,12 @@
 /// The pieces are an essential part to the game of chess. Their positions on the
 /// board and coordination dictate the behaviour of the game. The Pieces type
 /// stores the rules for each of the pieces and the valid moves of those pieces.
-protocol Pieces {
+protocol PiecesProtocol {
 
     /// The available positions where the piece can move on the board.
-    var validMoves: [Pieces] { get }
+    var validMoves: [Position] { get }
+
+    /// The current position of the piece on the board.
+    var currentPosition: Position { get }
 
 }
