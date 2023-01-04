8a4787ec5c3832f735607bfe876ee499939f9664
Karan Naidu <karan.naidu@gspacetech.com>
2023-01-04 16:58:13 +1000

    Created Player Colour type.

---
+++ Sources/App/PlayerColour.swift
@@ -0,0 +1,9 @@
+/// The Colour of the Player's chess pieces.
+enum PlayerColour {
+
+    /// Definition of the black set of chess pieces.
+    case black
+
+    /// Definition of the white set of chess pieces.
+    case white
+}
