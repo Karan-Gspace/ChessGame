3b045e1baaebfeb6f36669c415320db3a8ef0bcd
Karan Naidu <karan.naidu@gspacetech.com>
2023-01-05 21:32:36 +1000

    Moved ChessPieceType into a seperate file.

---
+++ Sources/App/ChessPieceType.swift
@@ -0,0 +1,22 @@
+/// Types of pieces that exist in chess.
+public enum ChessPieceType: String {
+
+    /// King piece
+    case king = "K"
+
+    /// Queen piece
+    case queen = "Q"
+
+    /// Bishop piece
+    case bishop = "b"
+
+    /// Knight piece
+    case knight = "k"
+
+    /// Rook piece
+    case rook = "r"
+
+    /// Pawn piece
+    case pawn = "p"
+
+}
